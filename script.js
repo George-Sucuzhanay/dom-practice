01c0f4ed71b1713c88196bac07f9d4f0ecbe8e0e
George <georgesucuzhanay100@gmail.com>
2021-10-17 23:28:14 -0400

    new functions created

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// #### Part 4
+
+// Select all `.special-title` class elements
+// change their `font-size` to `2rem`. Remember you might have to iterate through the list of elements
+
+// #### Part 5
+
+// Turns out DOM never raced in **Chicago**. Access the **Past Races** list and remove **Chicago**.
+
+// ## Creating New DOM Elements
+
+// #### Part 6
+
+// Let's add to DOM's **Past Races** list. Create a new `<li>` element, change the new `<li>` text to the name of a city, and append it to the **Past Races** list.
+
+// #### Part 7
+
+// Create a new `.blog-post` corresponding to the new city added in **Part 6**. You will have to create a new `<div>` with class of `.blog-post`, a new `<h1>` with text, and a new `<p>` with some text. Make the new element consistent with the other posts. Think about what order you want to create the elements, and what order you want to append them in.
+
+
 let quotes = [
   `I live my life a quarter mile at a time`,
   `I said a ten-second car, not a ten-minute car`,
@@ -20,16 +40,20 @@ window.onload =  function(event) {
   // Do all of your work inside the window.onload function (in other words, here!)
 
   // Part 1
-
+  document.getElementById("main-title").innerHTML = "Welcome to Tom's Website";
   // Part 2
-
+  document.querySelector("body").style.backgroundColor = "purple"
   // Part 3
-
+  let myList = document.getElementById("favorite-things")
+  myList.removeChild(myList.lastElementChild)
   // Part 4
-
+  let thisClass = document.getElementsByClassName("special-title")
+  for(i = 0; i < thisClass.length; i++){
+    thisClass.style.fontSize = "2rem";
   // Part 5
 
   // Part 6
 
   // Part 7
 }
+
